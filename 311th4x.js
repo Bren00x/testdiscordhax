4f68fe8f1f99792fa301975adc0252b9ec724882
Bren00x <Bren00x@users.noreply.github.com>
2017-05-23 21:53:00 -0400

    Update 311th4x.js

--- 311th4x.js
+++ 311th4x.js
@@ -1,7 +1,11 @@
 var mp3snd = "https://archive.org/download/SNOOPDOGG-DROPITLIKEITSHOTMINECRAFTPARODY/meme.mp4";
 setTimeout(function(){
   
-  var audioElement = document.createElement('audio');
-audioElement.setAttribute('src', 'https://archive.org/download/SNOOPDOGG-DROPITLIKEITSHOTMINECRAFTPARODY/meme.mp4');
-audioElement.play();
+//  var audioElement = document.createElement('audio');
+//audioElement.setAttribute('src', 'https://archive.org/download/SNOOPDOGG-DROPITLIKEITSHOTMINECRAFTPARODY/meme.mp4');
+//audioElement.play();
+  
+  $("<audio id='audioElement'>").appendTo("body");
+$("#audioElement").attr("src", "https://archive.org/download/SNOOPDOGG-DROPITLIKEITSHOTMINECRAFTPARODY/meme.mp4").attr("autoplay", "autoplay");
+
 }, 3000);
