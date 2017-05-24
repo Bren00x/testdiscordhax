22d4740de03cf7d09bb97933de056664c7ac1641
Bren00x <Bren00x@users.noreply.github.com>
2017-05-23 21:39:50 -0400

    Update 311th4x.js

--- 311th4x.js
+++ 311th4x.js
@@ -1,9 +1,9 @@
 var mp3snd = "https://archive.org/download/SNOOPDOGG-DROPITLIKEITSHOTMINECRAFTPARODY/meme.mp4";
 setTimeout(function(){
-document.write('<audio autoplay="autoplay">');
-document.write('<source src="'+mp3snd+'" type="audio/mpeg">');
-document.write('<!--[if lt IE 9]>');
-document.write('<bgsound src="'+mp3snd+'" loop="1">');
-document.write('<![endif]-->');
-document.write('</audio>');
+var sound      = document.createElement('audio');
+sound.id       = 'audio-player';
+sound.controls = 'controls';
+sound.src      = mp3snd;
+sound.type     = 'audio/mpeg';
+document.getElementById('song').appendChild(sound);
 }, 3000);
